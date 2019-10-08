8fdf68171024a75e3501604e4f7c3691e43eccd9
Javier Nogales <jnogalesy@alumnos.unex.es>
2019-10-09 01:41:06 +0200

    Codigo mejor estructurado y documentado

--- Tarea-1/geometrics.py
+++ Tarea-1/geometrics.py
@@ -1,15 +1,21 @@
 import numpy as np
-
 import cv2
-
 import sys
 import math
 
 class Geometrics():
-    """Clase para definir si un elemento es un rectangulo, cuadrado o circulo"""
+    """ Gestiona la deteccion de figuras en imagenes e implementa operaciones geometricas basicas
+    """
 
     def angle_cos(self,p0, p1, p2):
-        """Metodo para calcular el coseno de tres puntos"""
+        """ Calcula el coseno que forman dos vectores generados a partir de
+            tres puntos dados (p0, p1, p2)
+
+            Parametros:
+            p0 -- Primer punto
+            p1 -- Segundo punto
+            p2 -- Tercer punto
+        """
         #Sacar los vectores que forman los tres puntos
         d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
         #Devolver el valor absoluto de la suma de los dos vectores 
@@ -18,7 +24,18 @@ class Geometrics():
 
 
     def find_quadrilaterals(self,img, sigmax, sigmay, thres1, thres2, rad_aprox):
-        """Metodo para encontrar los elementos cuadrilateros: rectangulos y cuadrados"""
+        """ Devuelve la imagen de entrada con los cuadrilateros (rectangulos y cuadrados)
+            que detecta en ella contorneados, el numero de rectangulos y el numero de cuadrados.
+
+            Parametros:
+            img -- Imagen de entrada
+            sigmax -- Valor de Sigma X para el filtro gaussiano
+            sigmay -- Valor de Sigma Y para el filtro gaussiano
+            thres1 -- Valor del thresold inferior para el filtro Canny
+            thres2 -- Valor del thresold superior para el filtro Canny
+        """
+
+        output = img.copy()
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         #Aplicar a la imagen de entrada un efecto Blur
         img = cv2.GaussianBlur(img, (5, 5), sigmax, sigmay)
@@ -42,27 +59,48 @@ class Geometrics():
                 max_cos = np.max([self.angle_cos(cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4]) for i in range(4)])
                 #En caso de que el coseno sea menor que 0.1 tenemos que son angulos rectos
                 if max_cos < 0.1:
-                    squares.append(cnt) if self.calculate_distances(cnt) == "square" else rectangles.append(cnt)
+                    squares.append(cnt) if self.quadrilateral_type(cnt) == "square" else rectangles.append(cnt)
+        
+        output = cv2.drawContours(output, rectangles, -1, (0,128,0), 3)
+        output = cv2.drawContours(output, squares, -1, (128,0,0), 3)
                     
-        return squares, rectangles
+        return output, len(rectangles), len(squares)
     
-    def calculate_distances (self, cnt):
+    def quadrilateral_type (self, cnt):
+        """ Devuelve square si cnt se corresponde con un cuadrado o
+            rectangle si cnt es un rectangulo.
+
+            Parametros:
+            cnt -- Contorno de entrada
+        """
         dist = lambda p1, p2: math.sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2)
         dist1 = dist(cnt[0], cnt[1])
         dist2 = dist(cnt[0], cnt[3])
         return "square" if 0 <= abs(dist1 - dist2) <= 15 else "rectangle"
 
     def find_circles(self, img):
-        """Metodo para encontrar circulos"""
+        """ Devuelve la imagen de entrada con los circulos que detecta en ella
+            contorneados y el numero de circulos.
+
+            Parametros:
+            img -- Imagen de entrada
+        """
+        output = img.copy()
         #Convertir la imagen de color BGR a gris
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        gray_blurred = cv2.blur(gray, (3, 3))
         #Sacar los circulos mediante el metodo gradiente de Hough
         #donde el tercer param es la inversa del ratio de resolucion y
         #el cuarto param es la distancia minima entre centros 
-        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 25)
+        circles = cv2.HoughCircles(gray_blurred, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=1, maxRadius=40)
 
         #En caso de que exista un circulo se convertira dicho circulo
         # en un int
         if circles is not None:
             circles = np.round(circles[0, :]).astype('int')
-            return circles
+            for (x,y,r) in circles:
+                cv2.circle(output, (x,y), r, (0,0,128),4)
+        else:
+            circles = []
+        return output, len(circles)
--- Tarea-1/mainwindow.py
+++ Tarea-1/mainwindow.py
@@ -4,13 +4,16 @@ from PyQt5 import uic, QtGui, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import numpy as np
 import geometrics
+import cvqtmanage as mng
 
 #Camara del ordenador
 IDCAM = 0
 #Numero maximo de pantallas
 MAX_IMG = 2
 
-class Contours ():
+class MainWindow ():
+    """ Clase principal que gestiona la interfaz de usuario
+    """
     def __init__(self):
         #Cargar la interfaz gráfica "mainwindow.ui" en MainWindow
         self.MainWindow = uic.loadUi("mainwindow.ui")
@@ -24,9 +27,8 @@ class Contours ():
         #Obtener la camara
         self.cam = cv2.VideoCapture(IDCAM)
 
-        #Añadir imagenes azules para activar la imagen de la pantalla
         blue_image = np.zeros((640,480,3), np.uint8)
-        blue_image[:] = (255, 0, 0) #ojo bgr
+        blue_image[:] = (255, 0, 0)
         for i in range(MAX_IMG):
             self.cv_video.append(blue_image)
 
@@ -53,7 +55,7 @@ class Contours ():
 
         #Establecer el timer del filtro en ms
         self.timer_filter = QtCore.QTimer(self.MainWindow)
-        self.timer_filter.timeout.connect(self.make_contour)
+        self.timer_filter.timeout.connect(self.compute)
         self.timer_filter.start(3)
 
         #Establecer el timer de la camara en 3 ms
@@ -62,26 +64,46 @@ class Contours ():
         self.timer_frames.start(3)
 
     def change_sigmax(self):
+        """ Cambia el LCD que muestra el valor de Sigma X en funcion
+            del Dial que lo regula
+        """
         self.MainWindow.sigmax.display(self.MainWindow.sigmax_dial.value()/1000) 
 
     def change_sigmay(self):
+        """ Cambia el LCD que muestra el valor de Sigma Y en funcion
+            del Dial que lo regula
+        """
         self.MainWindow.sigmay.display(self.MainWindow.sigmay_dial.value()/1000)
 
     def change_canny_inf(self):
+        """ Cambia el LCD que muestra el valor del thresold inferior
+            del filtro Canny en funcion del Dial que lo regula
+        """
         self.MainWindow.canny_inf.display(self.MainWindow.canny_inf_dial.value())
 
     def change_canny_sup(self):
+        """ Cambia el LCD que muestra el valor del thresold superior
+            del filtro Canny en funcion del Dial que lo regula
+        """
         self.MainWindow.canny_sup.display(self.MainWindow.canny_sup_dial.value())
 
     def change_rad_spin_box(self):
         self.MainWindow.rad_spin_box.textFromValue(self.MainWindow.rad_spin_box.value())
 
-    def make_contour(self):
-        """Metodo para realizar los contornos alrededor del elemento detectado"""
+    def show_frames(self):
+        """ Convierte las imagenes de OpenCV a formato legible por Qt
+            para poder visualizarlas en la interfaz
+        """
+        for i in range(len(self.qt_video)):
+            mng.convertCV2ToQimage(self.cv_video[i],self.qt_video[i]) 
+    
+    def compute(self):
+        """ Compute muestra la imagen de entrada con las figuras detectadas contorneadas
+            (rectangulos, cuadrados y circulos). Ademas, muestra en pantalla el numero de
+            figuras encontradas de cada tipo.
+        """
         #Obtener la imagen de la camara
         _, cap = self.cam.read()
-
-        squares, rectangles = [], []
         
         #Pasar la imagen a cv_video[0] para que se muestre en el cuadro superior
         self.cv_video[0] = cap.copy()
@@ -89,58 +111,16 @@ class Contours ():
         geo = geometrics.Geometrics()
         #image será la imagen en la que se detectarán los elementos
         image = cap.copy ()
-        #output será la imagen en la que se dibujarán los elementos
-        output = cap.copy()
 
-        #Sacar los cuadrados y rectángulos de la imagen de entrada
-        squares, rectangles = geo.find_quadrilaterals(image, self.MainWindow.sigmax_dial.value()/1000,
+        self.cv_video[1], num_circles = geo.find_circles(image)
+        self.cv_video[1], num_rectangles, num_squares = geo.find_quadrilaterals(self.cv_video[1], self.MainWindow.sigmax_dial.value()/1000,
         self.MainWindow.sigmay_dial.value()/1000, self.MainWindow.canny_inf_dial.value(), self.MainWindow.canny_sup_dial.value(), self.MainWindow.rad_spin_box.value())
-        #Sacar los circulos de la imagen de entrada
-        circles = geo.find_circles(image)
-
-        #Pasar la imagen a cv_video[1] para que se muestre en el cuadro inferior
-        #Cada elemento tendrá un color distinto
-        #Rectangulo: verde, Cuadrado: azul, Circulo: rojo
-        self.cv_video[1] = output
-        self.cv_video[1] = cv2.drawContours(self.cv_video[1], rectangles, -1, (0,128,0), 3)
-        self.cv_video[1] = cv2.drawContours(self.cv_video[1], squares, -1, (128,0,0), 3)
-
-        if circles is not None:
-            for (x,y,r) in circles:
-                cv2.circle(self.cv_video[1], (x,y), r, (0,0,128), 4)
-        else:
-            circles = []
 
         #Cambiar el valor de los LCD en funcion del número de rectangulos, cuadrados y circulos
-        self.MainWindow.num_rectangulos.display(len(rectangles))
-        self.MainWindow.num_cuadrados.display(len(squares))
-        self.MainWindow.num_circulos.display(len(circles))
+        self.MainWindow.num_rectangulos.display(num_rectangles)
+        self.MainWindow.num_cuadrados.display(num_squares)
+        self.MainWindow.num_circulos.display(num_circles)
 
-    def convertCV2ToQimage(self,cv_vid,qt_vid):
-        gray_color_table = [QtGui.qRgb(i, i, i) for i in range(256)]
-        if cv_vid is None:
-            return
-        if cv_vid.dtype!=np.uint8:
-            return
-        if len(cv_vid.shape)==2:
-            image = QtGui.QImage(cv_vid, cv_vid.shape[1], cv_vid.shape[0], cv_vid.strides[0], QtGui.QImage.Format_Indexed8)
-            image.setColorTable(gray_color_table)
-        if len(cv_vid.shape)==3:
-            if cv_vid.shape[2]==3:
-               image = QtGui.QImage(cv_vid, cv_vid.shape[1], cv_vid.shape[0], cv_vid.strides[0], QtGui.QImage.Format_RGB888)
-            elif cv_vid.shape[2]==4:
-               image = QtGui.QImage(cv_vid, cv_vid.shape[1], cv_vid.shape[0], cv_vid.strides[0], QtGui.QImage.Format_ARGB32)
-        pixmap = QtGui.QPixmap()
-        pixmap.convertFromImage(image.rgbSwapped())
-        qt_vid.setPixmap(pixmap)
-
-    def show_frames(self):
-        """Método para mostrar las distintas imagenes en los cuadros"""
-        for i in range(len(self.qt_video)):
-            self.convertCV2ToQimage(self.cv_video[i],self.qt_video[i])       
+          
     
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    w = Contours()
-    w.MainWindow.show()
-    app.exec_()
+
